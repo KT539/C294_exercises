ee2099c0acb5fb0bf76e56e0a620b974a1702eab
Kilian Testard <pc47xiy@eduvaud.ch>
2025-11-03 14:51:37 +0100

    feat: finished exercise 2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
-<script setup></script>
+<script setup>
+  import { ref } from 'vue';
+  import testImage from './assets/images/socks_blue.jpeg';
+
+  const product = ref('Blue Socks')
+  const description = ref('A pair of blue socks with Vue Mastery logo')
+  const img = ref(testImage)
+  const url = ref('https://www.vuemastery.com/')
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{ product }}</h1>
+        <p>{{ description }}</p>
+        <a v-bind:href="url">Plus d'informations</a>
+        <img v-bind:src="img" />
       </div>
     </div>
   </div>
