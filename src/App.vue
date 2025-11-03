e3463dc0be0530d3667b556999e554fa6d35c1f3
Kilian Testard <pc47xiy@eduvaud.ch>
2025-11-03 16:30:17 +0100

    feat: finished exercise 4

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
+const details = ref(['50% coton', '30% laine', '20% polyester'])
+const variants = ref([{id:2234, color:'green'}, {id:2235, color:'blue'}])
+const sizes = ref(['S', 'M', 'L', 'XL'])
 
 </script>
   
@@ -16,6 +19,24 @@ const image = ref(socksGreenImage)
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <h2>Composition:</h2>
+        <ul>
+          <li>{{ details[0] }} </li>
+          <li>{{ details[1] }}</li>
+          <li>{{ details[2] }}</li>
+        </ul>
+        <h2>Available colors:</h2>
+        <ul>
+          <li v-for="variant in variants">
+            {{ variant.color }}
+          </li>
+        </ul>
+        <h2>Available sizes:</h2>
+        <ul>
+          <li v-for="size in sizes">
+            {{ size }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
