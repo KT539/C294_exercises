f03899f3ce5f0a98dd87bcf76376f9a852872ad8
Kilian Testard <pc47xiy@eduvaud.ch>
2025-11-10 08:54:45 +0100

    feat: finished exercise 5

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <script setup>
 import { ref } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
 const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const variants = ref([{id:2234, color:'green', image: socksGreenImage}, {id:2235, color:'blue', image: socksBlueImage}])
+const sizes = ref(['S', 'M', 'L', 'XL'])
 const cart = ref(0)
+
+function addToCart() {
+  cart.value++
+}
+function removeFromCart() {
+  cart.value--
+}
+function changeImage(variantImage) {
+  image.value = variantImage
+}
 </script>
   
 <template>
-  <div class="nav-bar"></div>
+  <div class="nav-bar">
+    <p>Cart : {{ cart }}</p>
+  </div>
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">    
-        <img v-bind:src="image">
+        <img v-bind:src="image" alt="product image">
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
@@ -26,6 +41,21 @@ const cart = ref(0)
             {{ detail }}
           </li>
         </ul>
+        <h2>Available colors:</h2>
+        <ul>
+          <li v-for="variant in variants"
+          @mouseover="changeImage(variant.image)">
+            {{ variant.color }}
+          </li>
+        </ul>
+        <h2>Available sizes:</h2>
+        <ul>
+          <li v-for="size in sizes">
+            {{ size }}
+          </li>
+        </ul>
+        <button @click="addToCart">Add To Cart</button>
+        <button @click="removeFromCart">Remove From Cart</button>
       </div>
     </div>
   </div>
