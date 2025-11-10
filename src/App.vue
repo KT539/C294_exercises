848ef7f461e4c922aa4efa17c923651926aea6b8
Kilian Testard <pc47xiy@eduvaud.ch>
2025-11-10 08:25:06 +0100

    feat: up to ex5

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ const product = ref('Socks')
 const image = ref(socksGreenImage)
 const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const cart = ref(0)
 </script>
   
 <template>
@@ -19,6 +20,12 @@ const details = ref(['50% cotton', '30% wool', '20% polyester'])
         <h1>{{ product }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
+        <h2>Composition:</h2>
+        <ul>
+          <li v-for="detail in details">
+            {{ detail }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
